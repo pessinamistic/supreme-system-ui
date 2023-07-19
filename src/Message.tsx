1b5d9069c4c02749439a979782d09a59a38aa101
pessinamistic <ssr3151@gmail.com>
2023-07-19 23:18:26 +0530

    Commit

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+// PascalCasing
+function Message(){
+    // JSX : JavaScript XML
+    const name = 'Shashank';
+    if (name)
+    return <h1> Hello {name}</h1>
+    return <h1> Hello Fucker</h1>
+
+}
+
+export default Message;
